a8dd12751491dde606112a6278e16041dfd561bc
Amit Rathore <amitrathore@gmail.com>
2013-03-28 16:03:42 -0700

    added lein-clojars to the project

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject zololabs/context-io "0.0.1"
   :description "Context.IO API wrapper for Clojure"
   :url "https://github.com/zololabs/context-io-clj"
-  :plugins [[lein-multi "1.1.0"]]
+  :plugins [[lein-clojars "0.9.1"]]
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/data.json "0.1.2"]
                  [clj-oauth "1.3.1-SNAPSHOT"]
