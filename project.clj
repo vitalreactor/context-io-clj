0e1e9688a81bb02a577f6f9dd97f7dc29244b273
Amit Rathore <amitrathore@gmail.com>
2013-03-28 23:16:57 -0700

    bumped to 0.0.4 and removed snapshot dependencies

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject zololabs/context-io "0.0.3"
+(defproject zololabs/context-io "0.0.4"
   :description "Context.IO API wrapper for Clojure"
   :url "https://github.com/zololabs/context-io-clj"
   :plugins [[lein-clojars "0.9.1"]]
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/data.json "0.1.2"]
-                 [clj-oauth "1.3.1-SNAPSHOT"]
+                 [clj-oauth "1.4.0"]
                  [http.async.client "0.4.5"]]
   :dev-dependencies [[lein-autodoc "0.9.0"]]
   :autodoc {:name "context-io-clj"
