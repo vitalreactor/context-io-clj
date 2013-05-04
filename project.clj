e7f191cf7405865834cc2b7c3b42842710f6a0c6
Siva Jagadeesan <sivajag@gmail.com>
2013-05-03 22:43:31 -0700

    using lein-sha to push git sha version jar to clojars

--- project.clj
+++ project.clj
@@ -1,11 +1,17 @@
 (defproject zololabs/context-io-clj "0.0.4"
   :description "Context.IO API wrapper for Clojure"
   :url "https://github.com/zololabs/context-io-clj"
-  :plugins [[lein-clojars "0.9.1"]]
+  
+  :plugins [[lein-clojars "0.9.1"]
+            [lein-sha-version "0.1.1"]]
+
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/data.json "0.1.2"]
                  [clj-oauth "1.4.0"]
                  [http.async.client "0.4.5"]]
-  :dev-dependencies [[lein-autodoc "0.9.0"]]
+
+  :profiles {:dev {:dependencies [[lein-autodoc "0.9.0"]]}
+             :sha {:length 7}}
+
   :autodoc {:name "context-io-clj"
             :description "Context.IO API wrapper for Clojure"})
